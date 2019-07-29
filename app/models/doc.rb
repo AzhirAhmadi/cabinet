dd43eccb7f3e3ca8f8c76b19f3b0428dbaa1ca4e
AzhirAhmadi <aaahmadi99@gmail.com>
2019-07-29 19:07:59 +0430

    add doc controller and model

---
+++ app/models/doc.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: docs
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Doc < ApplicationRecord
+end
